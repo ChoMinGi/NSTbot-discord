68ee5a82e51fe5ba45dce917bd89e65fae75b0d8
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-08-29 12:30:42 +0900

    Refactor calling class
    Client to Bot(extention)

--- main.py
+++ main.py
@@ -1,39 +1,29 @@
 import discord
 import asyncio
 from discord.ext import commands
-import requests
-import os
-from bs4 import BeautifulSoup
+from func import wisesaying
 
-client = discord.Client()
-
-token_path = os.path.dirname(os.path.abspath(__file__))+'/token.txt'
-get_token = open(token_path, "r", encoding="utf-8")
-token = get_token.read()
-client.run(token)
+bot = commands.Bot(command_prefix="!")
 
 
-def get_myeongan():
-    response = requests.get("http://munit.co.kr/lucky/today_proverb.php")
-    soup = BeautifulSoup(response.content.decode(
-        'utf-8', 'replace'), 'html.parser')
-    main = soup.find("p").text
-    if len(main) <= 100:
-        return(main)
-    else:
-        get_myeongan()
-
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
+
         return
 
     if message.content.startswith('!명언'):
-        quote = get_myeongan()
+        quote = wisesaying.wise()
         await message.channel.send(quote)
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('hello')
+
+
+bot.run(
+    'MTAwNDIxMjQ2MjU2MTc4ODA4NA.GIF_AA.mOhuc2GPNQlqMjbH7lWOtxtD3iubxlIdgTPuls')
