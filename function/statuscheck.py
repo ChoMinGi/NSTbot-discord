01ac7e7dc4b3793bbf04ebefc454839e7c5a2537
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-08-31 22:52:30 +0900

    Update statuscheck.py

--- function/statuscheck.py
+++ function/statuscheck.py
@@ -1,5 +1,6 @@
 import discord
 import wavelink
+from discord.ext import commands
 
 
 class state:
@@ -7,23 +8,30 @@ class state:
     async def UserInVC(self, ctx, bot):
         if ctx.author.voice:
             return True
-        await ctx.channel.send(f'{ctx.author.mention} 음성채널 에 없습니다.')
+        await ctx.channel.send(f'{ctx.author.mention} 음성채널에 없습니다.')
         return False
 
+    # check user not in voice channel
     async def UserNotInVC(self, ctx, bot):
         if ctx.author.voice:
             return False
         await ctx.channel.send(f'{ctx.author.mention} 음성채널 내에 있습니다.')
         return True
 
+    # check bot in voice channel
     async def BotInVC(self, ctx, bot):
-        if ctx.author.voice:
+        playuser = bot.wavelink.get_player(ctx.guild.id)
+
+        if playuser.is_connected:
             return True
-        await ctx.channel.send(f'{ctx.author.mention} 음성채널에 존재하지 않습니다.')
+        await ctx.channel.send(f'{ctx.author.mention} 봇이 음성채널 내에 없습니다.')
         return False
 
-    async def UserInVC(self, ctx, bot):
-        if ctx.author.voice:
+    # check bot not in voice channel
+    async def BotNotInVC(self, ctx, bot):
+        playuser = bot.wavelink.get_player(ctx.guild.id)
+
+        if not playuser.is_connected:
             return True
-        await ctx.channel.send(f'{ctx.author.mention} 음성채널에 존재하지 않습니다.')
+        await ctx.channel.send(f'{ctx.author.mention} 봇이 음성채널 내에 있습니다.')
         return False
