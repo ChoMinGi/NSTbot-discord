30ed6c393650ef7f912b25474e0bff0c11db9833
ChoMinGi <81455273+ChoMinGi@users.noreply.github.com>
2022-08-31 22:52:16 +0900

    Refactor main.py
    Adding wavelink

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 import discord
 import asyncio
 from discord.ext import commands
+import wavelink
 from function.wisesaying import wise
 from function.statuscheck import state
 
-bot = commands.Bot(command_prefix="!")
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(command_prefix='>?')
+
+    async def on_ready(self):
+        print('Bot is ready!')
 
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-
-        return
-
-    if message.content.startswith('!명언'):
-        quote = wise()
-        await message.channel.send(quote)
-
-    if message.content.startswith('!hello'):
-        await message.channel.send('hello')
-
-    if message.content.startswith('!join'):
-        a = state()
-        print(a)
-
-
-@bot.command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
-
-@bot.command()
-async def leave(ctx):
-    await ctx.voice_client.disconnect()
+bot = Bot()
 
 bot.run(
-    'MTAwNDIxMjQ2MjU2MTc4ODA4NA.GIF_AA.mOhuc2GPNQlqMjbH7lWOtxtD3iubxlIdgTPuls')
+    'MTAwNDIxMjQ2MjU2MTc4ODA4NA.GPV5jE.Dcv0Eih7OkgZ5t709pc5WTzYKALInCYqIBaa6Y')
